d89be3844350909d65bd31dc603280a3f525fb2b
guilherme nunes <guinucode@protonmail.com>
2021-10-15 18:09:49 -0300

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express, { Response } from "express";
+import path from "path";
 
 const router = express.Router();
 
-router.get("/", (_, res: Response) => res.status(200).send("success"));
+router.get("/", (_, res: Response) => {
+  res.status(200).sendFile("files/index.html", { root: "." });
+});
 
 export default router;
