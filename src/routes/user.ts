86fa9d9c7082ee0c5b082fa20462dd8c9abba691
Guilherme Nunes <techgn@protonmail.com>
2022-03-20 22:55:38 -0300

    Add storage functionality for user photos

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,19 +1,47 @@
-import express from "express";
-import userController from "../controllers/user";
+import express, { Request } from "express";
+import UserController from "../controllers/user";
+import multer from "multer";
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (
+    _req: Request,
+    _file: Express.Multer.File,
+    callback: Function
+  ) => {
+    callback(null, "photos/");
+  },
+  filename: async (
+    _req: Request,
+    file: Express.Multer.File,
+    callback: Function
+  ) => {
+    const fileType = file.originalname.split(".")[1];
+    const fileNewName = (await import("crypto"))
+      .randomBytes(10)
+      .toString("hex");
+
+    callback(null, `${fileNewName}.${fileType}`);
+  },
+});
+
+const upload = multer({ storage });
+
 router
-  .get("/", userController.getAll)
-  .get("/:id", userController.getById)
-  .post("/email", userController.verifyEmail)
-  .post("/login", userController.login)
-  .put("/", userController.create)
-  .patch("/:id", userController.update)
-  .patch("/:id/name", userController.updateName)
-  .patch("/:id/email", userController.updateEmail)
-  .patch("/:id/password", userController.updatePassword)
-  .patch("/:id/photo", userController.updatePhoto)
-  .delete("/:id", userController.exclude);
+  .get("/", UserController.getAll)
+  .get("/:id", UserController.getById)
+  .post("/email", UserController.getByEmail)
+  .post("/login", UserController.login)
+  .post("/email/check", UserController.checkIfEmailIsAvailable)
+  .post("/reset-password/send-code", UserController.resetPasswordSendCode)
+  .post("/email/verify", UserController.verifyEmailIfAvailable)
+  .put("/", UserController.create)
+  .patch("/:id", UserController.update)
+  .patch("/:id/name", UserController.updateName)
+  .patch("/:id/email", UserController.updateEmail)
+  .patch("/:id/password", UserController.updatePassword)
+  .patch("/:id/photo", upload.single("photo"), UserController.updatePhoto)
+  .delete("/:id", UserController.exclude);
 
 export default router;
