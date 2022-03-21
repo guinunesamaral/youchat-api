efbec1ae3768dc64df0753d126c29cc3a65dd61d
Guilherme Nunes <techgn@protonmail.com>
2022-03-21 09:08:50 -0300

    Add message router

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,16 +1,16 @@
 import express from "express";
-import messageController from "../controllers/message";
+import MessageController from "../controllers/message";
 
 const router = express.Router();
 
 router
-  .get("/", messageController.getAll)
-  .get("/:chat_id", messageController.getByChatId)
-  .get("/last/:chat_id", messageController.getLastMessageByChatId)
-  .put("/", messageController.create)
-  .patch("/:id/text", messageController.updateText)
-  .patch("/:id/star", messageController.star)
-  .patch("/:id/receive", messageController.receive)
-  .delete("/:id", messageController.exclude);
+  .get("/", MessageController.getAll)
+  .get("/:chat_id", MessageController.getByChatId)
+  .get("/last/:chat_id", MessageController.getLastMessageByChatId)
+  .put("/", MessageController.create)
+  .patch("/:id/text", MessageController.updateText)
+  .patch("/:id/star", MessageController.star)
+  .patch("/:id/receive", MessageController.receive)
+  .delete("/:id", MessageController.exclude);
 
 export default router;
